00e5fb5570cdf2d5ce9ba26f3b3ba72d89ae0197
muise001 <emiel.muis@hva.nl>
2019-03-21 10:55:06 +0100

    Addded Create User
    form and DB connection

--- site/app.js
+++ site/app.js
@@ -6,6 +6,9 @@ const app = express()
 const fetch = require("node-fetch")
 const mysql = require("promise-mysql")
 const ENV = require("dotenv").config().parsed
+const salt = "0923ur902hfwnWEhfewo89282@@@u9ef"
+const md5 = require("md5")
+// const session = require("express-session")
 
 const pool = mysql.createPool({
   host: ENV.DB_HOST,
@@ -19,6 +22,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
 
 app.get("/", (req, res) => {
   pool.query("SELECT * FROM test")
@@ -28,6 +36,25 @@ app.get("/", (req, res) => {
 
 app.get("/createAccount", (req, res) => {
   res.render("createAccount")
+})
+
+app.post("/login", (req, res) => {
+  const { password, email } = req.body
+  console.log(md5(salt+req.body.password+salt))
+})
+
+app.post("/createAccount", (req, res) => {
+  const { username, email, password, password2, name, last_name, birthDay, gender, bio, profilePicture } = req.body
+  if (password === password2) {
+    pool.query("INSERT INTO users SET ?", {
+      username, email, password: md5(salt+password+salt), name, last_name, birthDay, gender, bio, profilePicture
+    })
+    res.render("/home")
+  } else {
+    res.redirect("/?error=true")
+    console.log("passwords don't match")
+    return
+  }
 })
 
 app.post("/createUser", (req, res) => {
