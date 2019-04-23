a06ae77516446c8a6701cd11c75f5154f57ff91a
muise001 <emiel.muis@hva.nl>
2019-04-23 22:28:54 +0200

    Added ESlint
    Added a custom made es-lint config file and changed all .js files to likings of my linter

--- site/public/js/js.js
+++ site/public/js/js.js
@@ -1,14 +1,14 @@
-const signUpFormButtonsNext = document.querySelectorAll(".multiplePageForm a.next")
-const signUpFormButtonsPrevious = document.querySelectorAll(".multiplePageForm a.previous")
+const signUpFormButtonsNext = document.querySelectorAll(".multiplePageForm a.next");
+const signUpFormButtonsPrevious = document.querySelectorAll(".multiplePageForm a.previous");
 
 signUpFormButtonsNext.forEach((button, i) => {
   button.addEventListener("click", () => {
-    document.body.style.setProperty("--signUpFormPosish", `-${((i + 1) * 44)}vh`)
-  })
-})
+    document.body.style.setProperty("--signUpFormPosish", `-${((i + 1) * 44)}vh`);
+  });
+});
 
 signUpFormButtonsPrevious.forEach((button, i) => {
   button.addEventListener("click", () => {
-    document.body.style.setProperty("--signUpFormPosish", `-${(i * 44)}vh`)
-  })
-})
+    document.body.style.setProperty("--signUpFormPosish", `-${(i * 44)}vh`);
+  });
+});
